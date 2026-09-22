866c9307135d4bf141e379ae0d3a2953e4c2054b
Dominic Ebere <54512581+eldominics@users.noreply.github.com>
2026-09-23 01:56:42 +0800

    more changes to the codes of supabase folders cos I created a new supabase project

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,10 +1,6 @@
-1import { createBrowserClient } from "@supabase/ssr";
-2
-3const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-4const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-5
-6export const createClient = () =>
-7  createBrowserClient(
-8    supabaseUrl!,
-9    supabaseKey!,
-10  );
+import { createBrowserClient } from "@supabase/ssr";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const createClient = () =>
+  createBrowserClient(supabaseUrl!, supabaseKey!, 10);
